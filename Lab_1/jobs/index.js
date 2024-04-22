fc7a8ce5d41066cbf8033152b5e6851eb25345b8
kovalsofiia <koval.sofiia@student.uzhnu.edu.ua>
2024-04-22 15:54:05 +0300

    lab7

--- Lab_1/jobs/index.js
+++ Lab_1/jobs/index.js
@@ -1,15 +1,12 @@
 const config = require("../config");
 const startHeartBeatJob = require("./heartbeat.job");
-const startUserLoginReminderJob = require("./userLoginReminder.job");
 
 function start() {
   if (!config.enableScheduleJobs) {
     console.warn("Jobs scheduling is not enabled.");
     return;
   }
-
   startHeartBeatJob();
-  startUserLoginReminderJob();
 }
 
 module.exports = start;
